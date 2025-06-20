76c54c1954c949f6a0364878ce03311813c9ba8c
SissonJ <sissonjack67@gmail.com>
2025-06-20 10:09:12 -0500

    feat: add ibc forwarding

--- src/contract.rs
+++ src/contract.rs
@@ -1,8 +1,20 @@
 use cosmwasm_std::{
-    entry_point, to_binary, CosmosMsg, DepsMut, Env, MessageInfo, Response, StdResult,
+    entry_point, from_binary, to_binary, Addr, Coin, CosmosMsg, DepsMut, Env, IbcTimeout, MessageInfo, Response, StdResult
 };
+use crate::msg::{ExecMsg, InitMsg, RegisteredSnip20, Snip20HandleMsg, UnwrapTransfer};
+use cosmwasm_std::Storage;
 
-use crate::msg::{ExecMsg, InitMsg, Snip20HandleMsg};
+pub fn save_snip20(storage: &mut dyn Storage, key: &str, snip: &RegisteredSnip20) -> StdResult<()> {
+    storage.set(key.as_bytes(), &to_binary(snip)?);
+    Ok(())
+}
+
+pub fn load_snip20(storage: &dyn Storage, key: &str) -> StdResult<RegisteredSnip20> {
+    let data = storage.get(key.as_bytes()).ok_or_else(|| {
+        cosmwasm_std::StdError::generic_err("No snip20 registration found")
+    })?;
+    from_binary(&cosmwasm_std::Binary::from(data))
+}
 
 #[entry_point]
 pub fn instantiate(
@@ -15,7 +27,7 @@ pub fn instantiate(
 }
 
 #[entry_point]
-pub fn execute(_deps: DepsMut, _env: Env, info: MessageInfo, msg: ExecMsg) -> StdResult<Response> {
+pub fn execute(deps: DepsMut, env: Env, info: MessageInfo, msg: ExecMsg) -> StdResult<Response> {
     match msg {
         ExecMsg::WrapDeposit {
             snip20_address,
@@ -41,5 +53,52 @@ pub fn execute(_deps: DepsMut, _env: Env, info: MessageInfo, msg: ExecMsg) -> St
                 funds: vec![],
             }),
         ])),
+        ExecMsg::Receive(recv_msg) => {
+            if let Some(snip_msg) = recv_msg.msg {
+                let api = deps.api;
+                let decoded_msg = from_binary::<UnwrapTransfer>(&snip_msg)?;
+                let snip20 = load_snip20(deps.storage, &info.sender.to_string())?;
+                Ok(Response::default().add_messages([
+                    CosmosMsg::Wasm(cosmwasm_std::WasmMsg::Execute {
+                        contract_addr: snip20.snip20_address.clone().to_string(),
+                        code_hash: snip20.snip20_code_hash.clone(),
+                        msg: to_binary(&Snip20HandleMsg::Deposit { padding: None }).unwrap(),
+                        funds: info.funds.clone(),
+                    }),
+                    CosmosMsg::Ibc(cosmwasm_std::IbcMsg::Transfer {
+                        channel_id: decoded_msg.channel,
+                        to_address: decoded_msg.recipient_address,
+                        amount: Coin{
+                            amount: recv_msg.amount.clone(),
+                            denom: snip20.denom,
+                        },
+                        timeout: IbcTimeout::with_timestamp(env.block.time.plus_seconds(300)),
+                        memo: "".to_string(),
+                    })
+                ]))
+            } else {
+                Err(cosmwasm_std::StdError::generic_err("Invalid receive message"))
+            }
+        },
+        ExecMsg::RegisterSnip20 {
+            snip20_address,
+            snip20_code_hash,
+            denom,
+        } => {
+            let snip20 = RegisteredSnip20 {
+                snip20_address: Addr::unchecked(snip20_address),
+                snip20_code_hash,
+                denom,
+            };
+            save_snip20(deps.storage, &snip20.snip20_address.to_string(), &snip20)?;
+            Ok(Response::default().add_message(
+                CosmosMsg::Wasm(cosmwasm_std::WasmMsg::Execute {
+                    contract_addr: snip20.snip20_address.clone().to_string(),
+                    code_hash: snip20.snip20_code_hash.clone(),
+                    msg: to_binary(&Snip20HandleMsg::RegisterReceive{ code_hash: env.contract.code_hash, padding: None }).unwrap(),
+                    funds: vec![],
+                })
+            ))
+        },
     }
 }
--- src/msg.rs
+++ src/msg.rs
@@ -1,10 +1,26 @@
-use cosmwasm_std::Uint128;
+use cosmwasm_std::{Addr, Binary, Uint128};
+use cosmwasm_schema::cw_serde;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub struct InitMsg {}
+
+#[cw_serde]
+pub struct Snip20ReceiveMsg {
+    pub sender: String,
+    pub from: String,
+    pub amount: Uint128,
+    pub memo: Option<String>,
+    pub msg: Option<Binary>,
+}
+
+#[cw_serde]
+pub struct UnwrapTransfer {
+    pub channel: String,
+    pub recipient_address: String,
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
@@ -13,6 +29,12 @@ pub enum ExecMsg {
         snip20_address: String,
         snip20_code_hash: String,
         recipient_address: String,
+    },
+    Receive(Snip20ReceiveMsg),
+    RegisterSnip20 {
+        snip20_address: String,
+        snip20_code_hash: String,
+        denom: String,
     },
 }
 
@@ -23,6 +45,11 @@ pub enum Snip20HandleMsg {
     Deposit {
         padding: Option<String>,
     },
+    Redeem{
+        amount: Uint128,
+        denom: String,
+        padding: Option<String>,
+    },
     // Basic SNIP20 functions
     Transfer {
         recipient: String,
@@ -30,4 +57,15 @@ pub enum Snip20HandleMsg {
         memo: Option<String>,
         padding: Option<String>,
     },
+    RegisterReceive {
+        code_hash: String,
+        padding: Option<String>,
+    },
+}
+
+#[cw_serde]
+pub struct RegisteredSnip20 {
+    pub snip20_address: Addr,
+    pub snip20_code_hash: String,
+    pub denom: String,
 }
